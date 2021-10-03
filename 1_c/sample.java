76492630fee6171fbe7363c29e12b325d8e7d7f2
pavankumarallu <vamsipavankumarallu@gmail.com>
2021-10-03 17:02:28 +0530

    Renamed and added sample

---
+++ 1_c/sample.java
@@ -0,0 +1,9 @@
+/* This is a simple Java program.
+FileName : &quot;HelloWorld.java&quot;. */
+class sample {
+    // Your program begins with a call to main()
+    public static void main (String args[]) {
+        int a = 5;
+        a = a + 1;
+    }
+}
